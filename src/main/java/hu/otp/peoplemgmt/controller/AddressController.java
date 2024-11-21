d0b6730997859b989e00303cf43f3c914e838fb5
András Nyilas <15180993+b8nyiand@users.noreply.github.com>
2024-11-21 21:29:33 +0100

    * added controller layer

---
+++ src/main/java/hu/otp/peoplemgmt/controller/AddressController.java
@@ -0,0 +1,10 @@
+package hu.otp.peoplemgmt.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/address")
+public class AddressController {
+
+}
