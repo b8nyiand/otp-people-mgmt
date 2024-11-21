b122c294eaa57162d6e51452e1ee12f1ca19ccb6
András Nyilas <15180993+b8nyiand@users.noreply.github.com>
2024-11-21 12:15:05 +0100

    * updated .gitignore * added Spring Boot project skeleton * added necessary Gradle dependencies

---
+++ src/main/java/hu/otp/peoplemgmt/PeoplemgmtApplication.java
@@ -0,0 +1,13 @@
+package hu.otp.peoplemgmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeoplemgmtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeoplemgmtApplication.class, args);
+	}
+
+}
